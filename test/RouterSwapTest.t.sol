35665b73bd26a411359cdea57f5b80d779f9c16b
passer-byzhang <1541115709@qq.com>
2024-11-22 00:14:05 +0800

    add some test

--- test/RouterSwapTest.t.sol
+++ test/RouterSwapTest.t.sol
@@ -59,8 +59,117 @@ contract RouterSwapTest is Test {
         vm.stopBroadcast();
         assert(tokenB.balanceOf(alice) > 0);
         assert(tokenA.balanceOf(alice) == 0);
+    }
+
+
+    function testSwapTokensForExactTokens() public {
+        addLiquidity();
+        uint256 amountOut = 100 ether;
+        uint256 amountInMax = 1000 ether;
+        uint256 deadline = block.timestamp + 15;
+        tokenA.mint(alice, amountInMax);
+        vm.startBroadcast(alice);
+        tokenA.approve(address(router), amountInMax);
+        address[] memory path = new address[](2);
+        path[0] = address(tokenA);
+        path[1] = address(tokenB);
+        router.swapTokensForExactTokens(
+            amountOut,
+            amountInMax,
+            path,
+            alice,
+            deadline
+        );
+        vm.stopBroadcast();
+        assert(tokenB.balanceOf(alice) == amountOut);
+        assert(tokenA.balanceOf(alice) > 0);
+    }
+
+
+    function testSwapExactETHForTokens() public {
+        addLiquidityETH();
+        uint256 amountOutMin = 0;
+        uint256 deadline = block.timestamp + 15;
+        address[] memory path = new address[](2);
+        path[0] = address(weth);
+        path[1] = address(tokenA);
+        vm.startBroadcast(alice);
+        vm.deal(alice, 1000 ether);
+        router.swapExactETHForTokens{value: 10 ether}(
+            amountOutMin,
+            path,
+            alice,
+            deadline
+        );
+        vm.stopBroadcast();
+        assert(tokenA.balanceOf(alice) > 0);
 
     }
+    function testSwapTokensForExactETH() public {
+        addLiquidityETH();
+        uint256 amountOut = 100 ether;
+        uint256 amountInMax = 1000 ether;
+        uint256 deadline = block.timestamp + 15;
+        tokenA.mint(alice, amountInMax);
+        vm.startBroadcast(alice);
+        tokenA.approve(address(router), amountInMax);
+        address[] memory path = new address[](2);
+        path[0] = address(tokenA);
+        path[1] = address(weth);
+        router.swapTokensForExactETH(
+            amountOut,
+            amountInMax,
+            path,
+            alice,
+            deadline
+        );
+        vm.stopBroadcast();
+        assertEq(alice.balance,100 ether);
+    }
+
+    function testSwapExactTokensForETH() public {
+        addLiquidityETH();
+        uint256 amountIn = 100 ether;
+        uint256 amountOutMin = 0;
+        uint256 deadline = block.timestamp + 15;
+        tokenA.mint(alice, amountIn);
+        vm.startBroadcast(alice);
+        tokenA.approve(address(router), amountIn);
+        address[] memory path = new address[](2);
+        path[0] = address(tokenA);
+        path[1] = address(weth);
+        router.swapExactTokensForETH(
+            amountIn,
+            amountOutMin,
+            path,
+            alice,
+            deadline
+        );
+        vm.stopBroadcast();
+        assert(alice.balance>0);
+    }
+    
+    function testSwapETHForExactTokens() public {
+        addLiquidityETH();
+        uint256 amountOut = 100 ether;
+        uint256 amountInMax = 1000 ether;
+        uint256 deadline = block.timestamp + 15;
+        vm.startBroadcast(alice);
+        vm.deal(alice, 1000 ether);
+        address[] memory path = new address[](2);
+        path[0] = address(weth);
+        path[1] = address(tokenA);
+        router.swapETHForExactTokens{value: amountInMax}(
+            amountOut,
+            path,
+            alice,
+            deadline
+        );
+        vm.stopBroadcast();
+        assert(tokenA.balanceOf(alice) == amountOut);
+    }
+
+
 
     function addLiquidity() public {
         uint256 amountADesired = 1000 ether;
@@ -86,6 +195,25 @@ contract RouterSwapTest is Test {
                 deadline
             );
     }
-
+    function addLiquidityETH()public {
+        uint256 amountADesired = 1000 ether;
+        uint256 amountETHDesired = 1000 ether;
+        uint256 amountAMin = 900 ether;
+        uint256 amountETHMin = 900 ether;
+        uint256 deadline = block.timestamp + 15;
+        tokenA.mint(address(this), amountADesired);
+        tokenA.approve(address(router), amountADesired);
+        vm.deal(address(this), amountETHDesired);
+        // Add liquidity
+         router
+            .addLiquidityETH{value: 1000 ether}(
+            address(tokenA),
+            amountADesired,
+            amountAMin,
+            amountETHMin,
+            address(this),
+            deadline
+        );
+    }
 
 }
